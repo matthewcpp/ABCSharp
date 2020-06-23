2922b876ffaae7d3aaaaecd44610c2d303bcf067
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-06-23 08:47:30 -0700

    Cleanup info field parsing

---
+++ TestABC/TestParseTuneHeaderInfoFields.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ABC;
+
+namespace TestABC
+{
+    [TestClass]
+    public class TestParseTuneHeaderInfoFields
+    {
+        public void TitleInfoField()
+        {
+            var expectedTitle = "Hello World";
+            var abc = $"X:1\nT:{expectedTitle}";
+
+            var tune = Tune.Load(abc);
+            Assert.AreEqual(expectedTitle, tune.title);
+        }
+
+        [TestMethod]
+        public void ComposerInfoField()
+        {
+            var expectedComposer = "Matthew LaRocca";
+            var abc = $"X:1\nC:{expectedComposer}";
+
+            var tune = Tune.Load(abc);
+            Assert.AreEqual(expectedComposer, tune.header.composer);
+        }
+
+        [TestMethod]
+        public void ParseReferenceNumber()
+        {
+            var tune = Tune.Load("X:100");
+            Assert.AreEqual(100U, tune.header.referenceNumber);
+
+            tune = Tune.Load("X: 100");
+            Assert.AreEqual(100U, tune.header.referenceNumber);
+        }
+
+        [TestMethod]
+        public void InvalidReferenceNumber()
+        {
+            Assert.ThrowsException<ParseException>(() => Tune.Load("X:ZZZ"));
+        }
+
+        [TestMethod]
+        public void CannotAppearInTuneBody()
+        {
+            var infoFields = new List<string>()
+            {
+                "T:Hello World",
+                "C:Matthew LaRocca",
+                "X:1"
+            };
+
+            foreach (var infoField in infoFields)
+            {
+                var abc = $"CDEF | GABc\n{infoField}";
+                Assert.ThrowsException<ParseException>(() => { Tune.Load(abc); }, infoField);
+            }
+
+        }
+
+        [TestMethod]
+        public void CannotAppearInline()
+        {
+            var infoFields = new List<string>()
+            {
+                "T:Hello World",
+                "C:Matthew LaRocca",
+                "X:1"
+            };
+
+            foreach (var infoField in infoFields)
+            {
+                var abc = $"CDEF | [{infoField}] GABc";
+                Assert.ThrowsException<ParseException>(() => { Tune.Load(abc); }, infoField);
+            }
+        }
+    }
+}
